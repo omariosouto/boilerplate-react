5d35d7d77d2b210389703a29d20cb56bd7753d3c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-04-12 19:44:50 -0300

    update

--- src/diplomat/screens/HomeScreen/components/FetchUserData.test.tsx
+++ src/diplomat/screens/HomeScreen/components/FetchUserData.test.tsx
@@ -1,17 +1,26 @@
-import { FetchUserData } from "./FetchUserData";
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// ---------------
 import { httpMock } from "@commons/http-client/test-file";
+// ---------------
+import { FetchUserData } from "./FetchUserData";
 
 describe("<FetchUserData />", () => {
-  it("renders the component as expected", () => {
+  it("renders the component as expected", async () => {
 
-    httpMock.onGet("/users").reply(200, {
+    httpMock.onGet("https://api.github.com/users/omariosouto").reply(200, {
       "login": "omariosouto",
       "id": 13791385,
     });
 
     render(<FetchUserData />);
 
-    expect(true).toBe(true);
+    const $btn = await screen.findByRole("button");
+
+    await userEvent.click($btn);
+    
+    const $fetchContainer = await screen.findByText(/omariosouto/i);
+
+    expect($fetchContainer).toBeInTheDocument();
   });
 });
