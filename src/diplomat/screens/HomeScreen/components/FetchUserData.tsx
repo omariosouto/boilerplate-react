14c93cf49f19225392dc478c96b674b50cb20c40
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-04-15 14:22:50 -0300

    update

--- src/diplomat/screens/HomeScreen/components/FetchUserData.tsx
+++ src/diplomat/screens/HomeScreen/components/FetchUserData.tsx
@@ -47,3 +47,39 @@ export function FetchUserData() {
     </div>
   );
 }
+
+/*
+
+## Em caso de GET
+- useAsyncCall
+- Chama o adapter caso precise
+- Chama o controller
+  - O controller TEM que receber um objeto de domain, se ele receber alguma coisa estruturada
+  - Chama o httpClient
+    - No HttpClient, roda as validações que já temos hoje e chama um adapter pra converter de `wireIn` pra `DOMAIN`
+- Na tela, sempre pegamos o dado de domain e passamos propriedades especificas dos componentes e se possível, passamos a entidade toda pra baixo igual fazemos no back end
+  - Isso ajuda a gente a ser mais consistente, em casos de mudanças, adicionar algum atributo novo ou algo do genero. Dado que não temos uma API de Grafo, sempre pegamos a entidade.
+
+## Em caso de POST
+- onClick/onChange... 
+- Chama o controller
+  - O controller TEM que receber um objeto de domain, se ele receber alguma coisa estruturada
+    - Lembrar de ter um adapter de wire(tela)ToDomain(), pq o dado que vem da tela, pode não estar estruturado para o domain e precisamos garantir isso validando.
+    - Basicamente uma coerção do objeto
+
+
+// Overview sobre entidades
+interface Buyer {
+  id: UUID;
+}
+
+interface Buyer {
+  id: UUID;
+  name: string;
+}
+
+const buyer: Buyer = {};
+// -> No momento da venda, é só um ID
+// -> No momento do cadastro, é todos os campos necessários
+
+*/
